0a378780c47ae8a07cf6eb201737fa86e09571d2
cocky <smile.xiaohao@gmail.com>
2018-02-27 13:32:55 +0800

    Spring profile配置，区分线上与开发环境

--- src/main/java/com/cocky/profile/Main.java
+++ src/main/java/com/cocky/profile/Main.java
@@ -6,8 +6,11 @@ public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext();
+        //1、设置活动的profile
         context.getEnvironment().setActiveProfiles("prod");
+        //2、注册Bean配置类
         context.register(ProfileConfig.class);
+        //3、刷新容器
         context.refresh();
         DemoBean demoBean = context.getBean(DemoBean.class);
         System.out.println(demoBean.getContent());
